a479905afb600d81f765d65605e9f81390e1edc9
zhoudezhi <zhoudezhi@jd.com>
2017-02-06 18:13:20 +0800

    zhou

--- home/Gruntfile.js
+++ home/Gruntfile.js
@@ -3,7 +3,15 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
-   
+    //复制文件  
+    /*copy: {
+      images: {
+        expand: true, 
+        cwd: 'static/js/business/src/',
+        src: 'main.js', 
+        dest:'static/js/business/dist/'
+      }
+    },*/
 
     //将文件中定义的匿名函数转为具名函数
     transport: {  
@@ -11,15 +19,15 @@ module.exports = function(grunt) {
           options: {
             debug: false,
             // 生成具名函数的id的格式 默认值为 {{family}}/{{name}}/{{version}}/{{filename}} filename自动补全
-            idleading: '../static/dist/js/' 
+            idleading: '../js/business/src/' 
          },
           files: [{
             // 是否使用相对路径
             expand: true, 
             // 相对路径地址           
-            cwd:'static/src/js/',
+            cwd:'static/js/business/src/',
             // 需要生成具名函数的文件集合
-            src:['*.js','!main.js',],
+            src:['*.js','!main.js'],
             // 生成存放的文件目录
             dest:'.build'
           }]
@@ -30,15 +38,15 @@ module.exports = function(grunt) {
     concat: {
       js: {
           // the files to concatenate
-          src: ['static/dist/js/main.js','.build/*.js'],
+          src: ['static/js/business/dist/main.js','.build/*.js'],
           // the location of the resulting JS file
-          dest: 'static/dist/js/init.js'
+          dest: 'static/js/business/dist/init.js'
       },
       css: {
           // the files to concatenate  注意css的导入顺序，如果顺序无关，可以直接static/src/css/*.css
-          src: ['static/src/css/base.css','static/src/css/footer.css','static/src/css/header.css','static/src/css/menu.css','static/src/css/sidebar.css','static/src/css/index.css','static/src/css/index_1200.css'],
+          src: ['static/css/base.css','static/css/footer.css','static/css/header.css','static/css/menu.css','static/css/sidebar.css','static/css/index.css','static/css/index_1200.css'],
           // the location of the resulting css file
-          dest: 'static/dist/css/main.css'
+          dest: 'static/css/main.css'
       }
     },
 
@@ -54,14 +62,14 @@ module.exports = function(grunt) {
     //js压缩
     uglify: {
       options:{
-        stripBanners: true,
+        //stripBanners: true,
         banner:'/*<%= pkg.name %>-<%= pkg.version %>  <%= grunt.template.today("yyyy-mm-dd HH:MM:ss") %>*/\n'
       },              
       build: {
         files: {
-          'modules/seajs/2.2.0/sea.min.js': ['modules/seajs/2.2.0/sea.js'], 
-          'modules/jquery/jquery/3.0.0/jquery-3.0.0.min.js': ['modules/jquery/jquery/3.0.0/jquery-3.0.0.js'],
-          'static/dist/js/init.js': ['<%= concat.js.dest %>']   //把合并目录下的js压缩
+          //'static/js/lib/seajs/2.2.0/sea.min.js': ['static/js/lib/seajs/2.2.0/sea.js'], 
+          //'static/js/lib/jquery/jquery/3.0.0/jquery-3.0.0.min.js': ['static/js/lib/jquery/jquery/3.0.0/jquery-3.0.0.js'],
+          'static/js/business/dist/init.js': ['<%= concat.js.dest %>']   //把合并目录下的js压缩
         }
       }
     },
@@ -74,20 +82,11 @@ module.exports = function(grunt) {
       },
       build: {
         src: ['<%= concat.css.dest %>'],
-        dest: 'static/dist/css/main.min.css'
+        dest: 'static/css/main.min.css'
       }
     },
 
-     //复制文件  把src/images里面的图片复制到dist/images
-    copy: {
-      images: {
-        expand: true, 
-        cwd: 'static/src/images/',
-        src: '*', 
-        dest:'static/dist/images/'
-      }
-      
-    },
+    
 
     //删除临时目录
     clean: {
@@ -101,11 +100,11 @@ module.exports = function(grunt) {
   //grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-copy');
+  //grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-clean');
 
   
-
-  grunt.registerTask('default', ['copy','transport','concat','uglify','cssmin','clean']);
+ 
+  grunt.registerTask('default', ['transport','concat','uglify','cssmin','clean']);
 
 };
---
+++ home/static/js/business/dist/main.js
@@ -0,0 +1,36 @@
+
+define("../js/business/dist/init",["jquery"],function(require,exports,module) {
+	//方案一优于方案二，seajs.use原则只应该有一处，用于引用入口js文件。其他地方应用require来加载模块。
+
+	//方法一
+	var $ = require('jquery');
+	require('../src/lazyload.js');  
+	require('../src/public.js');
+	require('../src/index.js');
+
+	//require异步加载
+	require.async(['../src/slider_1.js','../src/slider_2.js'],function(){
+		$(".mainBanner").slider_1();
+    	$(".bannerSlier,.floorSlider").slider_2();
+	});
+
+	/*require('./slider_1.js');
+	require('./slider_2.js');*/
+
+
+	//方法二
+	/*seajs.use(['../static/src/js/lazyload.js','../static/src/js/public.js','../static/src/js/index.js'],function(){
+
+	});
+	
+	seajs.use(['jquery','../static/src/js/slider_1.js','../static/src/js/slider_2.js'],function($){
+	    $(".mainBanner").slider_1();
+	    $(".bannerSlier,.floorSlider").slider_2();
+	    
+	});*/
+
+	
+	
+
+
+});
