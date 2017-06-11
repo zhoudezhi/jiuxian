20d795041a01e24bd9fc435412052ebd5aa02fea
zhoudezhi <zhoudezhi@jd.com>
2017-06-11 20:29:32 +0800

    zhou

---
+++ baseFrame-seajs/static/js/config.js
@@ -0,0 +1,19 @@
+seajs.config({
+    base: '../js/',
+    alias: {         
+      'zepto': 'lib/zepto/1.2.0/zepto.min.js',
+      'template':'lib/template/2.0.4/template.min.js',
+      'templateNative':'lib/template/2.0.4/template-native.min.js',
+      'fastclick':'lib/fastclick/fastclick.min.js',
+      'md5':'tools/md5.min.js',
+      'swiper':'tools/swiper.min.js',
+      'helper':'common/helper.js',
+      'api':'common/api.js',
+      'dialog':'common/dialog.js',
+      'getLocation':'common/getLocation.js',
+      'goTop':'common/goTop.js',
+      'toast':'common/toast.js',
+      'utility':'common/utility.js',
+      'loadjs':'common/loadjs.js'
+    }
+});
